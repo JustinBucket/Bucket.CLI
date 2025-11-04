3b8ed507501b8a93e6d738795d60dd1ed2553baf
bucket <justinduffmailloux@gmail.com>
2025-11-04 02:28:49 -0500

    continuing work on GenerateTree

--- Bucket.CLI/Extensions.cs
+++ Bucket.CLI/Extensions.cs
@@ -13,20 +13,106 @@ namespace Bucket.CLI
         public const char DASH = '─';
         public const char PIPE = '│';
         // root
-        // ├── --command1 └ ├ │
-        // ├── --command2
+        // ├── command1 └ ├ │
+        // ├── command2
         // ├── financials
         // │   ├── fcommand1
         // │   │   └── reports
         // │   └── fcommand2
         // │       └── sales
-        // │           └── --scommand1
+        // │           └── scommand1
         // └── banking
-        //     ├── --bcommand1
-        //     └── --bcommand2
-        public static ICollection<string> GenerateTree(this Component component, int depth)
+        //     ├── bcommand1
+        //     └── bcommand2
+
+        // what if we had the top level build out the structure?
+        public static ICollection<string> GenerateTree(this Component component, int depth = 0)
+        {
+            // root
+            // ├── command1 └ ├ │
+            // ├── command2
+
+            // if we generate a dictionary of string, depth, does the top level have enough info to know which pipes to use?
+
+            // start with the root
+            var treeLines = new List<string>();
+            treeLines.Add(GenerateLeadingCharacters(depth) + component.GenerateInfo());
+
+            foreach (var child in component.Children)
+            {
+                treeLines.AddRange(child.GenerateTree(depth + 1));
+            }
+
+            // once the child lines have been added, go through each line in reverse order
+            // , changing TPIPES to LPIPES as needed
+
+            var reversedLines = new List<string>();
+
+            treeLines.Reverse();
+            var depthFixed = false;
+
+            foreach (var line in treeLines)
+            {
+                // want index for next depth down
+                var index = CalculateIndex(depth + 1);
+                // handles TPIPEs with children info on same line
+                if (line.Length >= index)
+                {
+                    if (
+                        line[index] == TPIPE
+                        && line[index + 1] == ' '
+                        && !depthFixed)
+                    {
+                        var modifiedLine = line.ToCharArray();
+                        modifiedLine[index] = ' ';
+                        reversedLines.Add(new string(modifiedLine));
+                    }
+                    // handles TPIPEs with nothing following
+                    else if (line[index] == TPIPE && !depthFixed)
+                    {
+                        var modifiedLine = line.ToCharArray();
+                        modifiedLine[index] = LPIPE;
+                        reversedLines.Add(new string(modifiedLine));
+                        depthFixed = true;
+                    }
+                    // handles TPIPEs at the start of child lines, between two children of lower depth
+                    else if (
+                        line[index] == TPIPE 
+                        && line[index + 1] == ' '
+                        && depthFixed)
+                    {
+                        var modifiedLine = line.ToCharArray();
+                        modifiedLine[index] = PIPE;
+                        reversedLines.Add(new string(modifiedLine));
+                    }
+                    else
+                    {
+                        reversedLines.Add(line);
+                    }
+                }
+                
+            }
+
+            // reverse again to make it normal
+            reversedLines.Reverse();
+
+            return reversedLines;
+        }
+        
+        private static int CalculateIndex(int depth)
+        {
+            // depth 1 = 0
+            // depth 2 = 4
+            // depth 3 = 8
+            // depth 4 = 12
+            var index = (depth - 1) * 4;
+            return index < 0 ? 0 : index;
+        }
+        public static ICollection<string> GenerateTree(this Component component, int depth, bool lastChild = false)
         {
             // TODO: now it's adding a pipe on top of everything else
+            // TODO: not putting enough space after children after depth 1
+            // for each depth beyond the first, add a pipe
             // does recursion make this impossible?
             // throw new NotImplementedException();
             var outputLines = new List<string>();
@@ -39,7 +125,15 @@ namespace Bucket.CLI
             else
             {
                 // first character is ├ pipe - unless it's the last child then it's └
-                infoString.Append(depth > 0 ? PIPE : TPIPE);
+                // space if lastchild
+                if (lastChild)
+                {
+                    infoString.Append(' ');
+                }
+                else
+                {
+                    infoString.Append(depth > 0 ? PIPE : TPIPE);
+                }
 
                 // next set is indent # - 1 x ─
                 // 0 spaces for first level (depth 1)
@@ -53,7 +147,7 @@ namespace Bucket.CLI
                 // 3 x depth + (depth - 1)
 
                 // 3 space, then 4 spaces for each depth after that
-                infoString.Append(new string(' ', CalculateNumberOfSpaces(depth)));
+                infoString.Append(GenerateLeadingCharacters(depth));
 
                 // then another tpipe for non-depth 0?
                 if (depth > 0)
@@ -77,12 +171,15 @@ namespace Bucket.CLI
                 // since the child line will be the next one after we add, store count to use as index
                 var lineCount = outputLines.Count;
 
+                // check if child is last
+                var isLastChild = i == component.Children.Count - 1;
+
                 // add output lines from children
-                outputLines.AddRange(component.Children[i].GenerateTree(depth + 1));
+                outputLines.AddRange(component.Children[i].GenerateTree(depth + 1, isLastChild));
 
                 // if we're on last child, turn TPIPE to LPIPE
                 // FOR THAT CHILD, NOT ITS CHILDREN
-                if (i == component.Children.Count - 1)
+                if (isLastChild)
                 {
                     // find last component at this depth and change TPIPE to LPIPE
                     // count is one higher than the index, since we want the next line this should just work
@@ -92,6 +189,29 @@ namespace Bucket.CLI
             }
 
             return outputLines;
+        }
+
+        private static string GenerateLeadingCharacters(int depth)
+        {
+            // if we're at depth 1, no pipe
+            // if we're at depth 2, pipe, but all dependent on whether we're on the the last child
+
+            if (depth == 0)
+            {
+                return string.Empty;
+            }
+
+            var numSpaces = CalculateNumberOfSpaces(depth);
+            var spaceString = new string(' ', numSpaces);
+            spaceString = spaceString + TPIPE + DASH + DASH + ' ';
+            if (depth > 1)
+            {
+                spaceString = spaceString.Insert(0, TPIPE.ToString());
+            }
+
+            // TODO: intersperse pipes for each depth level beyond 2
+            
+            return spaceString;
         }
         
         private static int CalculateNumberOfSpaces(int depth)
--- Testing/TestExtensions.cs
+++ Testing/TestExtensions.cs
@@ -14,17 +14,90 @@ namespace Testing
         public void TestTreeGeneration()
         {
             var rComp = new TestComponent("root", "root component");
-            var tComp = new TestComponent("test", "test component");
-            var tComp2 = new TestComponent("test2", "test component 2");
+            var command1 = new TestComponent("command1", "first command");
+            var command2 = new TestComponent("command2", "second command");
+            var financials = new TestComponent("financials", "financial commands");
+            var fcommand1 = new TestComponent("fcommand1", "first financial command");
+            var fcommand2 = new TestComponent("fcommand2", "second financial command");
+            var reports = new TestComponent("reports", "reports command");
+            var sales = new TestComponent("sales", "sales command");
+            var scommand1 = new TestComponent("scommand1", "sales subcommand 1");
+            var banking = new TestComponent("banking", "banking commands");
+            var bcommand1 = new TestComponent("bcommand1", "banking command 1");
+            var bcommand2 = new TestComponent("bcommand2", "banking command 2");
 
-            rComp.Children.Add(tComp);
-            rComp.Children.Add(tComp2);
+            rComp.Children.Add(command1);
+            rComp.Children.Add(command2);
+            rComp.Children.Add(financials);
+            financials.Children.Add(fcommand1);
+            fcommand1.Children.Add(reports);
+            financials.Children.Add(fcommand2);
+            fcommand2.Children.Add(sales);
+            sales.Children.Add(scommand1);
+            rComp.Children.Add(banking);
+            banking.Children.Add(bcommand1);
+            banking.Children.Add(bcommand2);
 
             var treeLines = rComp.GenerateTree(0);
 
             File.WriteAllLines("OutputTree.txt", treeLines);
             var outputTree = string.Join(Environment.NewLine, treeLines);
-            Assert.AreEqual("not eaqual", outputTree);
+            Assert.AreEqual("not equal", outputTree);
+        }
+
+        [TestMethod]
+        public void TestSimpleTreeGeneration()
+        {
+            var rComp = new TestComponent("root", "root component");
+            var command1 = new TestComponent("command1", "first command");
+            var command2 = new TestComponent("command2", "second command");
+            var financials = new TestComponent("financials", "financial commands");
+            var fcommand1 = new TestComponent("fcommand1", "first financial command");
+            var fcommand2 = new TestComponent("fcommand2", "second financial command");
+            var reports = new TestComponent("reports", "reports command");
+            var sales = new TestComponent("sales", "sales command");
+            var scommand1 = new TestComponent("scommand1", "sales subcommand 1");
+            var banking = new TestComponent("banking", "banking commands");
+            var bcommand1 = new TestComponent("bcommand1", "banking command 1");
+            var bcommand2 = new TestComponent("bcommand2", "banking command 2");
+
+            rComp.Children.Add(command1);
+            rComp.Children.Add(command2);
+            rComp.Children.Add(financials);
+            financials.Children.Add(fcommand1);
+            fcommand1.Children.Add(reports);
+            financials.Children.Add(fcommand2);
+            fcommand2.Children.Add(sales);
+            sales.Children.Add(scommand1);
+            rComp.Children.Add(banking);
+            banking.Children.Add(bcommand1);
+            banking.Children.Add(bcommand2);
+
+            var treeLines = rComp.GenerateTree();
+
+            File.WriteAllLines("OutputTree.txt", treeLines);
+            var outputTree = string.Join(Environment.NewLine, treeLines);
+            Assert.AreEqual("not equal", outputTree);
+        }
+
+        [TestMethod]
+        public void TestDepth2Generation()
+        {
+            var fcommand1 = new TestComponent("fcommand1", "first financial command");
+
+            var treeString = fcommand1.GenerateTree(2);
+
+            Assert.AreEqual("├   ├── fcommand1: first financial command", treeString.First());
+        }
+
+        [TestMethod]
+        public void TestDepth3GenerationNested()
+        {
+            var scommand1 = new TestComponent("scommand1", "sales subcommand 1");
+
+            var treeString = scommand1.GenerateTree(3);
+
+            Assert.AreEqual("│   │   └── sales: sales command", treeString.First());
         }
     }
 }
